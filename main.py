b224b9df00758b285ab11d7f042fe623a7a51518
salnyed <agorbuncova2004@gmail.com>
2023-12-15 23:05:15 +0400

    Added list_all_applications function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import subprocess
-import time
+import os
 
 
 def close_app(app):
@@ -29,3 +29,19 @@ def get_active_app_name():
     """
     output = subprocess.check_output(["osascript", "-e", script])
     return output.strip().decode("utf-8")
+
+
+def list_all_applications():
+    # Создаем пустой список для хранения имен приложений
+    apps = []
+    # Получаем путь к папке Applications
+    app_path = "/Applications"
+    # Перебираем все файлы в папке Applications
+    for file in os.listdir(app_path):
+        # Проверяем, является ли файл приложением (имеет расширение .app)
+        if file.endswith(".app"):
+            # Добавляем имя приложения в список
+            apps.append(file)
+    # Выводим список приложений на экран
+    for app in apps:
+        print(app)
