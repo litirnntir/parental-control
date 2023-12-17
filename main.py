30200b2a36d6cfac15d9383ed4b63a116ef594c8
salnyed <agorbuncova2004@gmail.com>
2023-12-17 15:55:36 +0400

    Added total time counter

--- main.py
+++ main.py
@@ -1,60 +1,56 @@
-# Импортируем необходимые модули
 import sys
 import subprocess
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
 from PyQt6.QtCore import QTimer
+import time
 
 
-# Создаем класс для нашего окна
+def get_active_app_name():
+    script = """
+    tell application "System Events"
+        set frontApp to name of first application process whose frontmost is true
+    end tell
+    return frontApp
+    """
+    output = subprocess.check_output(["osascript", "-e", script])
+    return output.strip().decode("utf-8")
+
+
 class AppTracker(QWidget):
     def __init__(self):
         super().__init__()
-        # Устанавливаем заголовок и размер окна
         self.setWindowTitle("App Tracker")
-        self.resize(300, 100)
-        # Создаем метки для отображения активного приложения и времени
-        self.app_label = QLabel("Active app: None")
-        self.time_label = QLabel("Time spent: 0 seconds")
-        # Создаем вертикальный слой для размещения меток
+        self.resize(300, 150)
+        self.app_label = QLabel("Активное приложение: None")
+        self.time_label = QLabel("Время в приложении: 0 seconds")
+        self.total_label = QLabel("Общее время: 0 seconds")
         self.layout = QVBoxLayout()
         self.layout.addWidget(self.app_label)
         self.layout.addWidget(self.time_label)
+        self.layout.addWidget(self.total_label)
         self.setLayout(self.layout)
-        # Создаем таймер для обновления данных каждую секунду
         self.timer = QTimer()
         self.timer.timeout.connect(self.update_data)
         self.timer.start(1000)
-        # Инициализируем переменные для хранения активного приложения и времени
         self.active_app = None
         self.time_spent = 0
+        self.total_time = 0
 
-    # Определяем функцию для получения активного приложения
-    def get_active_app_name(self):
-        script = """
-        tell application "System Events"
-            set frontApp to name of first application process whose frontmost is true
-        end tell
-        return frontApp
-        """
-        output = subprocess.check_output(["osascript", "-e", script])
-        return output.strip().decode("utf-8")
-
-    # Определяем функцию для обновления данных
     def update_data(self):
-        # Получаем активное приложение
-        current_app = self.get_active_app_name()
-        # Если оно изменилось, то сбрасываем время и обновляем метку
+        current_app = get_active_app_name()
         if current_app != self.active_app:
             self.active_app = current_app
             self.time_spent = 0
-            self.app_label.setText(f"Active app: {self.active_app}")
-        # Иначе увеличиваем время на одну секунду и обновляем метку
+            self.app_label.setText(f"Активное приложение: {self.active_app}")
         else:
             self.time_spent += 1
-            self.time_label.setText(f"Time spent: {self.time_spent} seconds")
+            time_str = time.strftime("%H:%M:%S", time.gmtime(self.time_spent))
+            self.time_label.setText(f"Время в приложении {self.active_app}: {time_str}")
+        self.total_time += 1
+        total_str = time.strftime("%H:%M:%S", time.gmtime(self.total_time))
+        self.total_label.setText(f"Общее время: {total_str}")
 
 
-# Создаем приложение и запускаем его
 app = QApplication(sys.argv)
 window = AppTracker()
 window.show()
